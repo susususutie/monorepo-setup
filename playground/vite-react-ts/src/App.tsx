428720d1169e35d79787affab8a3c2d96f5fe129
sutie <susususutie@gmail.com>
2025-05-24 01:06:09 +0800

    chore: 更新 vscode 配置

--- playground/vite-react-ts/src/App.tsx
+++ playground/vite-react-ts/src/App.tsx
@@ -3,8 +3,8 @@ import { useState, version } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {one, test } from 'pkg-other'
-import {two} from 'pkg-placeholder'
+import { one, test } from 'pkg-other'
+import { two } from 'pkg-placeholder'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,25 +12,23 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite@{viteVersion} + React@{version}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>
+        Vite@{viteVersion} + React@{version}
+      </h1>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR one:{test(one,1)} two:{two}
+          Edit <code>src/App.tsx</code> and save to test HMR one:{test(one, 1)} two:{two}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
